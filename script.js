512831b9e432f8de27de769548b4b2d86da1a328
Michael Carr <mcarr2468@gmail.com>
2020-09-16 12:05:08 -0500

    V1.1

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// $(document).ready(function() {
-//     $("#target1").hide()
-// });
-
-// $("#target1").css("color", "blue");
-
-
-alert('hey')
-
-console.log('hello')
-
-
+$(function() {
+    
+});
