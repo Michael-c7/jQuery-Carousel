5c457a4dea472cf65831681eceb7d3bf007326c4
Michael Carr <mcarr2468@gmail.com>
2020-09-19 14:11:07 -0500

    V1.4

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// $(function() {
-    
-// });
-
 /*Global variables */
 let allSlides = Array.from($(".slide"));
 let slides = document.querySelector(".slides");
@@ -11,11 +7,11 @@ let nextBtn = document.querySelector('.next-btn');
 let currentSlide = document.querySelector('.current-slide');
 let currentSlideWidth = currentSlide.getBoundingClientRect().width;
 
-// let nextSlide = currentSlide.nextElementSibling;
-// let previousSlide = currentSlide.previousElementSibling;
-
 let digitAndDotRegex = /[\d.]+/g;
-let widthArr = [];
+let widthArr = [0];
+
+
+
 
 
 function positionSlides(amountOfSlides = 0, currentSlideWidth) {
@@ -28,96 +24,45 @@ function positionSlides(amountOfSlides = 0, currentSlideWidth) {
 
 positionSlides(allSlides.length, currentSlideWidth);
 
-console.log(widthArr);
-
-
-let init = 0;
-
-
-// console.log(slideWidth)
-
-// function changeToPreviousSlide(event) {
-
-        // let previousSlideWidth = -widthArr[init];
-    // let slideWidth = -widthArr[init];
-
-    // change slide
-    // slides.style.transform = `translateX(${slideWidth}px)`;
-    // init--;
-    // add to the init value
-//     if(isNaN(slideWidth) === true || isNaN(init) === true) {
-//         init+=0;
-//     } else {
-//         init-=1;
-//     }
-
-//     if(slideWidth === -1) {
-//         init === 0;
-//     }
-
-
-//     console.log(slideWidth);
-    
-//     console.log(init);   
-// }
 
 
 
 
-// function changeToNextSlide(event) {
-        // let nextSlideValue = parseFloat(nextSlide.style.transform.match(digitAndDotRegex));
-        // let nextSlideWidth = -widthArr[init];
-    // let slideWidth = -widthArr[init];
-
-    // change slide
-    // slides.style.transform = `translateX(${slideWidth}px)`;
-    
-    // add to the init value
-//     if(isNaN(slideWidth) === true || isNaN(init) === true) {
-//         init+=0;
-//     } else {
-//         init+=1;
-//     }
-
-//     if(init === slideWidth ) {
-//         init === 0;
-//     }
-    
-//     console.log(slideWidth);
-    
-//     console.log(init);   
-// }       
-
-
-/*previous slides test */
-    // console.log(allSlides.length);
 let count = 0;
+
 function moveSlides(event) {
     let closestButton = event.target.closest("button");
-
+    // let slideWidth = -widthArr[count];
     // counting logic
 
         // prev slides stuff
         if(count > 0) { 
             if(closestButton === previousBtn) {
             count--;
+
         }
     }
+
+
         // next slides stuff
-        if(count < allSlides.length) { 
+        if(count < allSlides.length -1) { 
             if(closestButton === nextBtn) {
             count++;
+ 
         }
     }
 
     count+=0;
-    console.log(count);
+    
+    // change slide
+    slides.style.transform = `translateX(${-widthArr[count]}px)`;
 
+    console.log(count);
+    console.log(-widthArr[count]);
     }
-    
-    
-     
-    
+
+
+
 
 
 /*Event Listener */
@@ -125,31 +70,15 @@ previousBtn.addEventListener('click', moveSlides);
 nextBtn.addEventListener('click', moveSlides);
 
 
-/* TODO
-When do we want to iterate the count variable?
-- keep in 0 - 4 range 
-if it hits 0 or 4 can still iterate up or down 
-provided its going in the range
 
-*/
 
 
 /* 
-Known Bugs:(most severe to least severe)
-
-1. previous button broken(delayed)
-
-2. when you get to the very end
-of the previous slide & the next slide
-clicking buttons no longer move the slides
-
 ---
 STEPS
 1. position slides left to right X
 
 2. get next button working X
 
-3. get previous button working
-(copy of next button button just move slides 
-in opposite direction of the next button)
+3. get previous button working X
 */
